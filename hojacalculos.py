21c50c088391f0272588778f7124bdcecb744fbe
Gabriela Caceres <gagabrielacaceres@gmail.com>
2022-06-24 20:21:41 -0300

    agregué las tres comillas al final

--- hojacalculos.py
+++ hojacalculos.py
@@ -1,11 +1,11 @@
 '''x=100
 y=2
 print(x//y)
-print(x/y)'''
+print(x/y)
 r=2
 b=5
 c=3
 print(r**c*b)
 
 
-estoy escribiendo en el archivo de hojacalculo de la branch rama
+estoy escribiendo en el archivo de hojacalculo de la branch rama'''
